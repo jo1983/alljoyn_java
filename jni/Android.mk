c15da9d0d9beb34d4b1e9707e133218aa7bdf4bd
Greg Burns <gburns@quicinc.com>
2012-04-12 17:40:49 -0700

    Changes for bundled daemon

--- jni/Android.mk
+++ jni/Android.mk
@@ -1,16 +1,19 @@
 LOCAL_PATH := $(call my-dir)
-MBUS_DIST := $(LOCAL_PATH)/../../mbus/build/android/arm/$(APP_OPTIM)/dist
-MBUS_SRC := $(LOCAL_PATH)/../../mbus/src
+AJ_DIST := $(LOCAL_PATH)/../../mbus/build/android/arm/$(APP_OPTIM)/dist
+AJ_SRC := $(LOCAL_PATH)/../../mbus/src
 
 include $(CLEAR_VARS)
 
 LOCAL_MODULE    := alljoyn_java
 LOCAL_SRC_FILES := alljoyn_java.cc
 LOCAL_CPP_EXTENSION := .cc
-LOCAL_PRE_CXXFLAGS := -I$(MBUS_DIST)/inc/stlport
-LOCAL_C_INCLUDES := $(MBUS_DIST)/inc $(MBUS_SRC)
+LOCAL_PRE_CXXFLAGS := -I$(AJ_DIST)/inc/stlport
+LOCAL_C_INCLUDES := $(AJ_DIST)/inc $(AJ_SRC)
 LOCAL_CFLAGS := -DQCC_OS_GROUP_POSIX -DQCC_OS_ANDROID -DQCC_CPU_ARM -DANDROID 
-LOCAL_LDLIBS := $(MBUS_DIST)/lib/liballjoyn.a \
+LOCAL_LDLIBS := \
+	$(AJ_DIST)/lib/liballjoyn.a \
+	$(AJ_DIST)/lib/libajdaemon.a \
+	$(AJ_DIST)/lib/BundledDaemon.o \
 	-L$(ANDROID_SRC)/out/target/product/generic/system/lib -lcrypto -llog
 
 include $(BUILD_SHARED_LIBRARY)
