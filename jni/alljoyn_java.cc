0665c90ef0c943cbfb215b8c46b45be4ae598d15
Nikhil <ndabhade@quicinc.com>
2012-08-13 00:22:48 -0700

    ALLJOYN-1112: Proximity Service changes for bundled daemon case on Android

--- jni/alljoyn_java.cc
+++ jni/alljoyn_java.cc
@@ -33,6 +33,7 @@
 #include <MsgArgUtils.h>
 #include <SignatureUtils.h>
 #include "alljoyn_java.h"
+#include <ProximityScanner.h>
 
 #define QCC_MODULE "ALLJOYN_JAVA"
 
@@ -761,6 +762,15 @@ static jmethodID MID_MsgArg_marshal_array = NULL;
 static jmethodID MID_MsgArg_unmarshal = NULL;
 static jmethodID MID_MsgArg_unmarshal_array = NULL;
 
+// This is used by the Proximity Scanner since it makes calls in the
+// Java framework for wifi scan results
+extern JavaVM *proxJVM;
+extern JNIEnv* psenv;
+extern jclass CLS_AllJoynAndroidExt;
+extern jmethodID MID_AllJoynAndroidExt_Scan;
+extern jclass CLS_ScanResultMessage;
+
+
 /**
  * Get a valid JNIEnv pointer.
  *
@@ -817,6 +827,8 @@ JNIEXPORT jint JNICALL JNI_OnLoad(JavaVM* vm,
     if (jvm->GetEnv((void**)&env, JNI_VERSION_1_2)) {
         return JNI_ERR;
     } else {
+    	proxJVM = jvm;
+    	psenv = env;
         jclass clazz;
 
         clazz = env->FindClass("java/lang/Integer");
@@ -932,6 +944,22 @@ JNIEXPORT jint JNICALL JNI_OnLoad(JavaVM* vm,
             return JNI_ERR;
         }
         CLS_SessionOpts = (jclass)env->NewGlobalRef(clazz);
+
+        clazz = env->FindClass("org/alljoyn/bus/ScanResultMessage");
+        if (!clazz) {
+        	return JNI_ERR;
+        }
+        CLS_ScanResultMessage = (jclass)env->NewGlobalRef(clazz);
+
+        clazz = env->FindClass("org/alljoyn/bus/AllJoynAndroidExt");
+        if (!clazz) {
+        	return JNI_ERR;
+        }
+        CLS_AllJoynAndroidExt = (jclass)env->NewGlobalRef(clazz);
+        MID_AllJoynAndroidExt_Scan = env->GetStaticMethodID(CLS_AllJoynAndroidExt, "Scan", "(Z)[Lorg/alljoyn/bus/ScanResultMessage;");
+        if (!MID_AllJoynAndroidExt_Scan) {
+        	return JNI_ERR;
+        }
 
         return JNI_VERSION_1_2;
     }
