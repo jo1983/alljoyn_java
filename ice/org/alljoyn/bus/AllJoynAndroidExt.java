1fbb25676fc3eb4ccac9b93d478e92a01fb67bbd
Nikhil <ndabhade@quicinc.com>
2012-08-28 15:39:37 -0700

    ALLJOYN-1322: Android bundled daemon does not crash when an application's AndroidManifest.xml does not have android.permission.CHANGE_WIFI_STATE This permission is required so that we can issue an explicit Wifi scan through the Android framework in case of bundled daemon. If a developer forgets to mention it will throw an exception stating that the permissions to run discovery over ICE is missing. If this permission is not mentioned the proximity module will report only the access point to which it is connected, if any

--- ice/org/alljoyn/bus/AllJoynAndroidExt.java
+++ ice/org/alljoyn/bus/AllJoynAndroidExt.java
@@ -87,51 +87,62 @@ public class AllJoynAndroidExt{
 		// else
 		// Request a scan
 		//
-		Log.v(TAG,"Calling updateTimer() -=-=-=-=-=-=-=-=-=-=-=-=-=-");
-		Log.v(TAG,"lastScanTime-=-=-=-=-=-=-=-=-=-=-=-=-=-"+lastScanTime);
-		currentTime = System.currentTimeMillis();
-		Log.v(TAG,"currentTime-=-=-=-=-=-=-=-=-=-=-=-=-=-"+currentTime);
-		long timeSinceLastScan = currentTime - lastScanTime;
-		Log.v(TAG,"timeSinceLastScan-=-=-=-=-=-=-=-=-=-=-=-=-=-"+timeSinceLastScan);
-		if(timeSinceLastScan >= 60000){
-			if(!wifiMgr.isWifiEnabled() || !wifiMgr.startScan()){
-				Log.v(TAG,"startScan() returned error");
-				lastScanTime = System.currentTimeMillis();
+		try{
+			Log.v(TAG,"Calling updateTimer() -=-=-=-=-=-=-=-=-=-=-=-=-=-");
+			Log.v(TAG,"lastScanTime-=-=-=-=-=-=-=-=-=-=-=-=-=-"+lastScanTime);
+			currentTime = System.currentTimeMillis();
+			Log.v(TAG,"currentTime-=-=-=-=-=-=-=-=-=-=-=-=-=-"+currentTime);
+			long timeSinceLastScan = currentTime - lastScanTime;
+			Log.v(TAG,"timeSinceLastScan-=-=-=-=-=-=-=-=-=-=-=-=-=-"+timeSinceLastScan);
+			if(timeSinceLastScan >= 60000){
+				if(!wifiMgr.isWifiEnabled() || !wifiMgr.startScan()){
+					Log.v(TAG,"startScan() returned error");
+					lastScanTime = System.currentTimeMillis();
+				}
+				tHandler.postDelayed(tUpdateTimerRunnable, 60000);
+			} else {
+				Log.v(TAG,"60secs not over. Sleeping again -=-=-=-=-=-=-=-=-=-=-=-=-=-");
+				tHandler.postDelayed(tUpdateTimerRunnable, 60000 - timeSinceLastScan);
 			}
-			tHandler.postDelayed(tUpdateTimerRunnable, 60000);
-		} else {
-			Log.v(TAG,"60secs not over. Sleeping again -=-=-=-=-=-=-=-=-=-=-=-=-=-");
-			tHandler.postDelayed(tUpdateTimerRunnable, 60000 - timeSinceLastScan);
+		} catch (SecurityException se){
+			Log.e(TAG,"Possible missing permissions needed to run ICE over AllJoyn. Discovery over ICE in AllJoyn may not work due to this");
+			se.printStackTrace();
 		}
 	}
 	
 	public AllJoynAndroidExt(Context sContext){
 		ctx = sContext;
 		wifiMgr = (WifiManager)ctx.getSystemService(Context.WIFI_SERVICE);
-		if(!wifiMgr.startScan()){
-			Log.v(TAG,"startScan() returned error");
-		}
 		
-		if(tHandler == null){
-		tHandler = new Handler();
-		} 
-	
-		tUpdateTimerRunnable = new Runnable() {
-
-			public void run() {
-				// TODO Auto-generated method stub
-				updateTimer();
+		try{
+			if(!wifiMgr.startScan()){
+				Log.v(TAG,"startScan() returned error");
 			}
-		};
+			
+			if(tHandler == null){
+			tHandler = new Handler();
+			} 
+		
+			tUpdateTimerRunnable = new Runnable() {
 	
-	tHandler.postDelayed(tUpdateTimerRunnable, 0);
-
-	if(alarmMgr == null){
-		alarmMgr = (AlarmManager)ctx.getSystemService(Context.ALARM_SERVICE);
-		Intent alarmIntent = new Intent(ctx, ScanResultsReceiver.class);
-		PendingIntent pi = PendingIntent.getBroadcast(ctx, 0, alarmIntent, 0);
-		alarmMgr.setRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis(), 1000 * 60 * 10, pi); // Millisec * Second * Minute
-	}
+				public void run() {
+					// TODO Auto-generated method stub
+					updateTimer();
+				}
+			};
+		
+		tHandler.postDelayed(tUpdateTimerRunnable, 0);
+	
+		if(alarmMgr == null){
+			alarmMgr = (AlarmManager)ctx.getSystemService(Context.ALARM_SERVICE);
+			Intent alarmIntent = new Intent(ctx, ScanResultsReceiver.class);
+			PendingIntent pi = PendingIntent.getBroadcast(ctx, 0, alarmIntent, 0);
+			alarmMgr.setRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis(), 1000 * 60 * 10, pi); // Millisec * Second * Minute
+		}
+		} catch (SecurityException se){
+			Log.e(TAG,"Possible missing permissions needed to run ICE over AllJoyn. Discovery over ICE in AllJoyn may not work due to this");
+			se.printStackTrace();
+		}
 	}
 	
 	public static void PrepareScanResults(){
@@ -172,97 +183,102 @@ public class AllJoynAndroidExt{
 				return scanResultMessageToBeSent;
 			}
 			// Else wifi is turned on and we can proceed with the scan
-			if(scanResultMessage == null && request_scan){
-				Log.v(TAG,"***************************Requested Scan Results**************************************");
-				// Only a start scan or timeout scan can restart the scan processing so we set this boolean
-				// stopScanRequested = false;
-				
-					Log.v(TAG," =-=-=-Scan-=-=-= Requested");
-					wifiMgr = (WifiManager)ctx.getSystemService(Context.WIFI_SERVICE);
-					if(wifiMgr == null){
-						Log.v(TAG," Wifi Manager is NULL");	
-					}
-					boolean wifiEnabled = wifiMgr.isWifiEnabled(); 
+			try{
+				if(scanResultMessage == null && request_scan){
+					Log.v(TAG,"***************************Requested Scan Results**************************************");
+					// Only a start scan or timeout scan can restart the scan processing so we set this boolean
+					// stopScanRequested = false;
 					
-					// This handles wifi not being enabled and hotspot being enabled
-					
-					if(!wifiEnabled){
-						// return empty map since wifi is not enabled
-						Log.v(TAG," Wifi not enabled");
-						scanResultMessage = new ScanResultMessage[0];
-						return scanResultMessage; 
-					} else{
-						Log.v(TAG," Wifi is enabled");
-					}
-					
-					scanResultsObtained = false;
-	
-//					if(receiver == null){
-//						// Pass the map and the boolean scanResultsObtained here and use the same map to form the return message 
-//						receiver = new ScanResultsReceiver(this);
-//						registerReceiver(receiver, new IntentFilter(
-//								WifiManager.SCAN_RESULTS_AVAILABLE_ACTION));
-//					}
-					
-					if(!wifiMgr.startScan()){
-						Log.v(TAG,"startScan() returned error");
-					}
-					else {
-						Log.v(TAG,"startScan() call was successful");
-					}
-					
-					// Check the boolean passed to the ScanResultsReceiver
-					// If it was set then you can return the result 
-					// Note : It can be the case that the scan did not return any results 
-					while(true){
-						Log.v(TAG,"Waiting for scanResultsObtained");
-						if(scanResultsObtained){
-							break;
+						Log.v(TAG," =-=-=-Scan-=-=-= Requested");
+						wifiMgr = (WifiManager)ctx.getSystemService(Context.WIFI_SERVICE);
+						if(wifiMgr == null){
+							Log.v(TAG," Wifi Manager is NULL");	
 						}
-						else{
-							try{	
-								Thread.sleep(5000);
-							}catch(InterruptedException ie){
-								Log.v(TAG, "Thread was interrupted while it was sleeping");
+						boolean wifiEnabled = wifiMgr.isWifiEnabled(); 
+						
+						// This handles wifi not being enabled and hotspot being enabled
+						
+						if(!wifiEnabled){
+							// return empty map since wifi is not enabled
+							Log.v(TAG," Wifi not enabled");
+							scanResultMessage = new ScanResultMessage[0];
+							return scanResultMessage; 
+						} else{
+							Log.v(TAG," Wifi is enabled");
+						}
+						
+						scanResultsObtained = false;
+		
+	//					if(receiver == null){
+	//						// Pass the map and the boolean scanResultsObtained here and use the same map to form the return message 
+	//						receiver = new ScanResultsReceiver(this);
+	//						registerReceiver(receiver, new IntentFilter(
+	//								WifiManager.SCAN_RESULTS_AVAILABLE_ACTION));
+	//					}
+						
+						if(!wifiMgr.startScan()){
+							Log.v(TAG,"startScan() returned error");
+						}
+						else {
+							Log.v(TAG,"startScan() call was successful");
+						}
+						
+						// Check the boolean passed to the ScanResultsReceiver
+						// If it was set then you can return the result 
+						// Note : It can be the case that the scan did not return any results 
+						while(true){
+							Log.v(TAG,"Waiting for scanResultsObtained");
+							if(scanResultsObtained){
+								break;
+							}
+							else{
+								try{	
+									Thread.sleep(5000);
+								}catch(InterruptedException ie){
+									Log.v(TAG, "Thread was interrupted while it was sleeping");
+								}
 							}
 						}
+						//PrepareScanResults();
+				}
+				else{
+					//
+					// If ScanResultMessage is NULL and a scan was not requested then we need to check it we are still connected to a  
+					// network. 
+					// If Yes then we need to add this network to the scan results that are returned
+					// If Not then we return what we have already received
+					//
+					
+					// There is a corner case here where you are not connected to any network and Scan() function is called when 
+					// it is ok to have scanResultMessage = null so we check if we are connected to any network first
+					
+					boolean isConnected = false;
+					
+					boolean wifiEnabled = wifiMgr.isWifiEnabled(); 
+					
+					if(wifiEnabled && wifiMgr.getConnectionInfo() != null){
+						isConnected = true;
 					}
-					PrepareScanResults();
-			}
-			else{
-				//
-				// If ScanResultMessage is NULL and a scan was not requested then we need to check it we are still connected to a  
-				// network. 
-				// If Yes then we need to add this network to the scan results that are returned
-				// If Not then we return what we have already received
-				//
-				
-				// There is a corner case here where you are not connected to any network and Scan() function is called when 
-				// it is ok to have scanResultMessage = null so we check if we are connected to any network first
-				
-				boolean isConnected = false;
-				
-				boolean wifiEnabled = wifiMgr.isWifiEnabled(); 
-				
-				if(wifiEnabled && wifiMgr.getConnectionInfo() != null){
-					isConnected = true;
-				}
+							
+					if(scanResultMessage == null && isConnected){
+						scanResultMessage = new ScanResultMessage[1];
+						scanResultMessage[0] = new ScanResultMessage();
 						
-				if(scanResultMessage == null && isConnected){
-					scanResultMessage = new ScanResultMessage[1];
-					scanResultMessage[0] = new ScanResultMessage();
-					
-					String currentBSSID = wifiMgr.getConnectionInfo().getBSSID();
-					String currentSSID = wifiMgr.getConnectionInfo().getSSID();
-					
-					scanResultMessage[0].bssid = currentBSSID;
-					scanResultMessage[0].ssid = currentSSID;
-					scanResultMessage[0].attached = true;
+						String currentBSSID = wifiMgr.getConnectionInfo().getBSSID();
+						String currentSSID = wifiMgr.getConnectionInfo().getSSID();
+						
+						scanResultMessage[0].bssid = currentBSSID;
+						scanResultMessage[0].ssid = currentSSID;
+						scanResultMessage[0].attached = true;
+					}
+					Log.v(TAG,"*************************** NOT REQUESTED Scan Results**************************************");
+					//PrepareScanResults();
 				}
-				Log.v(TAG,"*************************** NOT REQUESTED Scan Results**************************************");
-				PrepareScanResults();
+			}catch (SecurityException se){
+				Log.e(TAG,"Possible missing permissions needed to run ICE over AllJoyn. Discovery over ICE in AllJoyn may not work due to this");
+				se.printStackTrace();
 			}
-		
+			PrepareScanResults();
 			scanResultMessage = null;
 			
 			Log.v(TAG,"************************FINAL SCAN RESULTS ****************************************");
